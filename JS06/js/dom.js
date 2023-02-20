e71c8cbedc37aa28abd3c5a9f98b2fd852acbbbc
recillas4 <recillasjorge4@gmail.com>
2023-02-19 22:09:13 -0800

    DOM JS06

--- JS06/js/dom.js
+++ JS06/js/dom.js
@@ -90,3 +90,78 @@ var botonDivision = document.getElementById("botonDivision");
 
 
 var resultado = document.getElementById("resultado")
+
+function suma(){
+    let valor1 = parseInt(input1.value);//pido num1
+    let valor2 = parseInt(input2.value);
+    let suma = valor1 + valor2; //suma
+    resultado.innerHTML = suma; //pongo el resultado en us lugar
+}
+function resta(){
+    let valor1 = parseInt(input1.value);//pido num1
+    let valor2 = parseInt(input2.value);//pido num2
+    let resta = valor1 - valor2;
+    resultado.innerHTML = resta;
+
+}
+function multiplicacion(){
+    let valor1 = parseInt(input1.value);//pido num1
+    let valor2 = parseInt(input2.value);//pido num2
+    let multiplicacion = valor1 * valor2;
+    resultado.innerHTML = multiplicacion;
+
+}
+function division(){
+    let valor1 = parseInt(input1.value);//pido num1
+    let valor2 = parseInt(input2.value);//pido num2
+    let division = valor1 / valor2;
+    resultado.innerHTML = division;
+
+}
+
+/*como se crea un evento (addEventListenner)
+    -node.addEventListener ("evento a escuchar quee quiero k haga")
+    node donde aterrizo el evento
+    addlistener usa el elemento
+    evento a escuchar click mouseover etc
+    lo k quiero k haga, la llamada de la funcion
+    */
+
+botonSuma.addEventListener("click", suma)
+botonResta.addEventListener("click", resta)
+botonMultiplicacion.addEventListener("click",multiplicacion)
+botonDivision.addEventListener("click",division)
+
+
+/*
+    METODOS
+
+    acceeder a elementos
+    -document.getElementById(\)
+    -document.getElementByTagName(<\>)
+    -document.getElementByClassName
+
+    crear elementos
+    -document.createElement(etiqueta)
+    -document.createTextNode (texto) investigar
+    
+    insertar elementos
+        -parentElement.append 
+        -parentElement.insertBefore
+        -parentElement.insertAdjacentElement
+
+    modificar elementos
+
+    -node.outerHTML (Leer o referenciar elementp)
+    - node.innerHTML (modificar elemento)
+*/
+
+//primer paso definir con que voy a interactuar almaceno en vaiable
+const textoAModificar = document.querySelector('#h1')
+
+
+
+//creo funvion que cambia de color swgun el color que le paso como param
+function cambiaColor(color){
+    textoAModificar.style.color = color;
+}
