63f33a48ba21a07c36ab537daaa850830ba7b600
recillas4 <recillasjorge4@gmail.com>
2023-03-07 09:27:35 -0600

    java 09 OOP

---
+++ 05 java/Java09/oop/src/testers/TestFecha.java
@@ -0,0 +1,27 @@
+package testers;
+
+import oop.clases.Fecha;
+
+public class TestFecha {
+
+	public static void main(String[] args) {
+Fecha date = new Fecha();
+	date.setDia(6);
+	date.setMes(3);
+	date.setAnio(2023);
+	
+System.out.println(	date.diaMesAniio());	
+System.out.println("---> " + date);
+
+
+
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
